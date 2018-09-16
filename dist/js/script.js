0037866b10a50b34a4ac1a02e7063b911c2ec14e
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2018-09-17 02:00:16 +0300

    Finished the initial website for the first time.

---
+++ dist/js/script.js
@@ -0,0 +1,14 @@
+var myEmail = "saleem.abushelbayeh@gmail.com"
+
+function showEmail() {
+    var newPara = document.createElement("p");
+    var newContent = document.createTextNode(myEmail);
+
+    newPara.appendChild(newContent);
+
+    document.body.appendChild(newPara);
+}
+// document.getElementById("showEmailButton").onclick = showEmail();
+
+
+
