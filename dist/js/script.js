9f1cbac47a92085c5b2b35748debd0ee4ad1cda5
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2019-01-02 15:32:19 +0200

    Changed how the email is displayed

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,5 @@
-var myEmail = "saleem.abushelbayeh@gmail.com";
-
+const myEmail = "saleem.abu.shelbayeh@gmail.com";
+var counter = 0;
 // function showEmail() {
 //     var newPara = document.createElement("p");
 //     var newContent = document.createTextNode(myEmail);
@@ -11,7 +11,15 @@ var myEmail = "saleem.abushelbayeh@gmail.com";
 // document.getElementById("showEmailButton").onclick = showEmail();
 
 function showEmail() {
-    document.getElementById("myEmail").style.display = "block";
+    //document.getElementById("myEmail").style.display = "block";
+    if (counter === 0) {
+        const newPara = document.getElementById("myEmail");
+        var newContent = document.createTextNode(myEmail);
+
+        newPara.appendChild(newContent);
+        counter++;
+    }
+    
 }
 
 
